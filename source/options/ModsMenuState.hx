84b040f6779ce12e1900efa49c3925fd090e977b
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-07-12 06:34:19 +0800

    Update ModsMenuState.hx

--- source/options/ModsMenuState.hx
+++ source/options/ModsMenuState.hx
@@ -378,6 +378,10 @@ class ModsMenuState extends MusicBeatState
 
 		FlxG.mouse.visible = true;
 
+		#if android
+                addVirtualPad(UP_DOWN, B);
+                #end
+		
 		super.create();
 	}
 
